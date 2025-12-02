af4e74346715acc6e2c8c0b3554aae70b384139a
santiagoccaa <santiagoccaa@gmail.com>
2025-12-01 19:05:51 -0500

    add zustand

---
+++ store/contextPorpofolio.ts
@@ -0,0 +1,26 @@
+import { create } from "zustand";
+
+interface PorfolioState {
+    page: number;
+    size: number;
+
+    currentModule: string | null;
+
+    setPage: (value: number) => void;
+    setSize: (value: number) => void;
+
+    setModule: (module: string) => void;
+    reset: () => void;
+}
+
+export const usePorfolioStore = create<PorfolioState>((set, get) => ({
+    page: 1,
+    size: 10,
+    currentModule: null,
+
+    setPage: (value) => set({ page: value }),
+    setSize: (value) => set({ size: value }),
+
+    setModule: (module) => set({ currentModule: module }),
+    reset: () => set({ page: 1, size: 10 })
+}));
