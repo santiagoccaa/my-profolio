0b76c02e7fe648b525402f6d15157e5afa5f9132
santiagoccaa <santiagocontrerasarrieta@gmail.com>
2026-04-29 17:34:50 -0500

    navbar done

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,7 +2,7 @@
 
 import Link from "next/link"
 import { usePathname } from "next/navigation"
-import { FaHome } from "react-icons/fa"
+import { useState } from "react"
 import { GiSkills } from "react-icons/gi"
 import { GrProjects } from "react-icons/gr"
 import { MdConnectWithoutContact, MdMiscellaneousServices, MdPersonSearch } from "react-icons/md"
@@ -10,11 +10,6 @@ import { SiXdadevelopers } from "react-icons/si"
 
 const nav = [
 
-    {
-        hero: 'home',
-        href: '/',
-        icon: <FaHome />
-    },
     {
         name: 'projects',
         href: '/projects',
@@ -48,20 +43,28 @@ const nav = [
 ]
 
 const Navbar = () => {
+    const [active, setActive] = useState(0)
+
     const pathName = usePathname()
     if (pathName === '/') return
     return (
-        <div className='w-fit px-4 py-2 rounded-full fixed left-1/2 overflow-hidden -translate-x-1/2 bottom-4 z-40 flex items-center gap-4'>
-            <div className="absolute w-full h-full bg-primary opacity-50 top-0 left-0" />
-            {
-                nav.map(({ href, icon }, index) => (
-                    <Link key={index} href={href} className="relative">
-                        <span className={`${pathName === href && 'text-primary'} transition-all duration-300 text-2xl`}>
+        <div className=' fixed left-1/2 -translate-x-1/2 bottom-4 z-40 w-fit'>
+            <div className="relative bg-neutral-800 rounded-full px-2 py-2 flex gap-2 shadow-lg">
+                <span
+                    className="absolute top-2 h-10 w-10 rounded-full bg-primary transition-all duration-500 ease-out"
+                    style={{
+                        transform: `translateX(${active * 120}%)`,
+                    }}
+                />
+                {
+                    nav.map(({ href, icon }, index) => (
+                        <Link key={index} href={href} onClick={() => setActive(index)}
+                            className={`relative z-10 min-w-10 h-10 rounded-full flex items-center justify-center text-sm font-medium transition-colors duration-300 cursor-pointer ${active === index ? "text-white" : "text-primary"} text-xl`}>
                             {icon}
-                        </span>
-                    </Link>
-                ))
-            }
+                        </Link>
+                    ))
+                }
+            </div>
         </div>
     )
 }
